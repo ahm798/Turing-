c5bfd9ffa22705556dd6f7fdbdc5ad418c18e661
ahmed <ahm.798@pm.me>
2022-06-03 21:24:15 +0200

    build article model

--- article/models.py
+++ article/models.py
@@ -1,3 +1,56 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import Q
+from django.utils import timezone
+
+
+
+class ModelQuerySet(models.QuerySet):
+    def is_published(self):
+        return self.filter(status='published')
+
+    def search(self, query, user=None):
+        lookup = Q(title__icontains=query) | Q(content__icontains=query)
+        qs = self.is_published().filter(lookup)
+        if user is not None:
+            qs = qs.filter(owner__username=user)
+        return qs
+
+
+class ArticleManger(models.Manager):
+    def get_queryset(self, *args, **kwargs):
+        return ModelQuerySet(self.model, using=self.db)
+
+    def search(self, query, user=None):
+        return self.get_queryset().is_published().search(query=query, user=user)
+
+
+
+
+class Article(models.Model):
+    STATUS_CHOICES = (('draft', 'Draft'), ('published', 'Published'))
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='articles')
+    title = models.CharField(max_length=500, default="untitled")
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    content = models.TextField()
+    #
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    objects = ArticleManger()
+
+    def is_published(self) -> bool:
+        return self.status == 'published'
+
+    #
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
