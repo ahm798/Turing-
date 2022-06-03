8b601c4ffbefd2534d0fef21f67b295b01134c00
ahmed <ahm.798@pm.me>
2022-06-03 23:15:36 +0200

    build user profile model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
-# Create your models here.
+
+# user interested topic
+class TopicTag(models.Model):
+    name = models.CharField(primary_key=True, max_length=150, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+# user skills
+class SkillTag(models.Model):
+    name = models.CharField(primary_key=True, max_length=150, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+# _____________________________User profile model__________________________________
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=True)
+    username = models.CharField(max_length=200, null=True)
+    profile_pic = models.ImageField(blank=True, null=True, default='default.png')
+    bio = models.TextField(null=True)
+    vote_ratio = models.IntegerField(blank=True, null=True, default=0)
+    followers_count = models.IntegerField(blank=True, null=True, default=0)
+    skills = models.ManyToManyField(SkillTag, related_name='personal_skills', blank=True)
+    interests = models.ManyToManyField(TopicTag, related_name='topic_interests', blank=True)
+    followers = models.ManyToManyField(User, related_name='following', blank=True)
+    email_verified = models.BooleanField(default=False)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.user.username)
