a51bde79a3de48d2fddba8bc5512a9435d0c2fcb
ahmed <ahm.798@pm.me>
2022-06-03 21:49:10 +0200

    article list view

--- article/views.py
+++ article/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework import status
+from django.db.models import Q
+from .models import Article, ArticleComment, ArticleVote
+from .serializers import ArticleSerializer, ArticleCommentSerializer
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated
+from account.models import TopicTag
 
-# Create your views here.
+
+#list articles
+@api_view(['GET'])
+@permission_classes((IsAuthenticated,))
+def articleListView(request):
+    query = request.query_params.get('q')
+    if query is None:
+        query = ''
+    articles = Article.objects.filter(Q(content__icontains=query) | Q(title__icontains=query)).order_by("-created")
+    paginator = PageNumberPagination()
+    paginator.page_size = 10
+    result_page = paginator.paginate_queryset(articles, request)
+    serializer = ArticleSerializer(result_page, many=True)
+    return paginator.get_paginated_response(serializer.data)
