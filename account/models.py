212ddf416f7e91597a8d0f400989167043d31940
ahmed <ahm.798@pm.me>
2022-06-04 16:04:03 +0200

    finsh search client

--- account/models.py
+++ account/models.py
@@ -21,6 +21,7 @@ class SkillTag(models.Model):
 
 # _____________________________User profile model__________________________________
 class UserProfile(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=200, null=True)
     username = models.CharField(max_length=200, null=True)
@@ -32,7 +33,6 @@ class UserProfile(models.Model):
     interests = models.ManyToManyField(TopicTag, related_name='topic_interests', blank=True)
     followers = models.ManyToManyField(User, related_name='following', blank=True)
     email_verified = models.BooleanField(default=False)
-    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
     def __str__(self):
         return str(self.user.username)
