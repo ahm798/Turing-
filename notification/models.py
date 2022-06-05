8b1cf4f7eeed41af41e1ee9350878db4f2f498f0
ahmed <ahm.798@pm.me>
2022-06-05 12:33:13 +0200

    build notification model and create serializers file

--- notification/models.py
+++ notification/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
-# Create your models here.
+from article.models import Article
+from feed.models import Feed
+
+
+class Notification(models.Model):
+    CHOICES = (
+        ('article', 'article'),
+        ('feed', 'feed'),
+        ('follow', 'follow'),
+    )
+
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    target = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name='notifications')
+    created = models.DateTimeField(auto_now_add=True)
+    source = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    content = models.CharField(max_length=255)
+    readed = models.BooleanField(default=False)
+    notification_type = models.CharField(max_length=20, choices=CHOICES)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, null=True, blank=True)
+    feed = models.ForeignKey(Feed, on_delete=models.CASCADE, null=True, blank=True)
+    followed_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name='followed_by')
+
+    def __str__(self):
+        return self.content
